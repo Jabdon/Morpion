c79d069f7a9c67d1e9f84a8f119b000ccbe050fd
Jonathan Abdon <abdonjonathan@gmailcom>
2016-07-23 07:53:46 -0400

    Now, Grid renders programtically

--- app/src/main/java/com/example/jonathan/morpion/game_screen.java
+++ app/src/main/java/com/example/jonathan/morpion/game_screen.java
@@ -1,5 +1,11 @@
 package com.example.jonathan.morpion;
 
+import android.annotation.TargetApi;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.graphics.drawable.GradientDrawable;
 
 public class game_screen extends AppCompatActivity {
 
@@ -24,26 +31,34 @@ public class game_screen extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //draw grid in layout
         createGridsheet(gridview);
+        LinearLayout game_container = (LinearLayout) findViewById(R.id.game_container);
+        //game_container.setBackgroundColor(Color.BLACK);
 
     }
 
     // this method will populate button in grid section
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     protected void createGridsheet(Square[][] matrix){
-        //Create gridview
-        GridLayout layout = new GridLayout(this);
-        layout.setColumnCount(10);
-        layout.setRowCount(10);
-        layout.setLayoutParams(new ViewGroup.LayoutParams(-1 , -1));
+        GridLayout layout = (GridLayout) findViewById(R.id.gridlayout_screen);
+
         for(int i = 0; i < matrix.length ; i++){
             for(int j = 0; j <matrix[i].length; j++ ){
                 Square sqr = new Square(this) ;
+                GradientDrawable gd = new GradientDrawable() ;
+                gd.setStroke(1, Color.BLACK);
+                LinearLayout.LayoutParams sqrlayout = new LinearLayout.LayoutParams(90, 90);
+                sqr.setLayoutParams(sqrlayout);
+                sqr.setBackground(gd);
+                matrix[i][j] = sqr ;
+
                 // add it to grid view
                 layout.addView(matrix[i][j]);
+                //System.out.println("the height for " + "Matrix " + i +"/"+ j +" is " + sqr.getHeight());
             }
         }
-        //add this in the main view
-        LinearLayout game_container = (LinearLayout) findViewById(R.id.game_container);
-        game_container.addView(layout);
+
+
+        System.out.println("should be added to game screen");
 
     }
 
