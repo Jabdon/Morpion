f8771653857fe02dfe07c9016e6c4597a6356a53
Jonathan Abdon <abdonjonathan@gmailcom>
2016-08-04 19:50:14 -0400

    - add thread to handle algorithm that calculates whether it is Morpion

--- app/src/main/java/com/example/jonathan/morpion/game_screen.java
+++ app/src/main/java/com/example/jonathan/morpion/game_screen.java
@@ -7,8 +7,10 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -25,8 +27,10 @@ import android.graphics.drawable.GradientDrawable;
 
 public class game_screen extends AppCompatActivity {
 
-    public Square [][] gridview = new Square[10][10] ;
+    public static Square [][] gridview = new Square[10][10] ;
     public static int current_player = 1 ;
+    public static Thread thread_morpion ;
+    public static boolean is_morpion = false ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +53,7 @@ public class game_screen extends AppCompatActivity {
 
         for(int i = 0; i < matrix.length ; i++){
             for(int j = 0; j <matrix[i].length; j++ ){
-                Square sqr = new Square(this) ;
+                Square sqr = new Square(this, i, j) ;
                 GradientDrawable gd = new GradientDrawable() ;
                 gd.setColor(Color.rgb(218, 218, 218));
                 GridLayout.LayoutParams sqrlayout = new GridLayout.LayoutParams();
@@ -103,6 +107,216 @@ public class game_screen extends AppCompatActivity {
         );
 
         return px ;
+    }
+    /*
+    this method will determine whether or not it morpion
+    meaning if five squares with the same color are aligned (vertically, horizontally & diagonaly)
+    int row - row position of the most recent placed square
+    int column - column position of the most recent placed square
+    Return type - returns true if it exists such alignmemnt, false if not
+     */
+
+    public static boolean isMorpion( final int row, final int column, final int side){
+
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("the row is " + row + " And the column " + column + " with state " + side);
+                        int count;
+                        //boolean to check
+                        boolean not_done_checking_horizontally_right = true;
+                        boolean not_done_checking_horizontally_left = true;
+                        boolean not_done_checking_vertically_up = true;
+                        boolean not_done_checking_vertically_down = true;
+                        boolean not_done_checking_diagonally_up_right = true;
+                        boolean not_done_checking_diagonally_up_left = true;
+                        boolean not_done_checking_diagonally_down_right = true;
+                        boolean not_done_checking_diagonally_down_left = true;
+
+                        //code to check if it is aligned horizontally
+                        if(!is_morpion){
+                            count = 1;
+                            int column_step = column ;
+                            while(not_done_checking_horizontally_right){
+                                column_step++;
+                                if(column_step <= 9 && gridview[row][column_step].state == side){
+                                    count++ ;
+                                    if(count == 5){
+                                        is_morpion = true ;
+                                        System.out.println("ok it is Morpion here 1");
+                                        break ;
+                                    }
+                                }
+                                else{
+                                    // stop this loop and go check for left
+                                    not_done_checking_horizontally_right = false ;
+                                    column_step = column ;
+                                }
+
+                            }
+                            System.out.println("the row is " + row + " And the column " + column + " with state " + side);
+                            while(not_done_checking_horizontally_left){
+                                column_step-- ;
+                                if(column_step >= 0 && gridview[row][column_step].state == side){
+                                    count++ ;
+                                    if(count == 5){
+                                        // game is over, stop this process immediatelly
+                                        is_morpion = true ;
+                                        System.out.println("ok it is Morpion here 2");
+                                        break ;
+                                    }
+                                }
+                                else{
+                                    // stop this loop and go check vertically
+                                    not_done_checking_horizontally_left = false ;
+                                }
+
+                            }
+                        }
+
+                        //code to check if it is aligned vertically
+                        if(!is_morpion){
+                            count = 1;
+                            int row_step = row ;
+                            while(not_done_checking_vertically_up){
+                                row_step++;
+                                if(row_step <= 9 && gridview[row_step][column].state == side){
+                                    count++ ;
+                                    if(count == 5){
+                                        is_morpion = true ;
+                                        System.out.println("ok it is Morpion here 3");
+                                        break ;
+                                    }
+                                }
+                                else{
+                                    // stop this loop and go check for left
+                                    not_done_checking_vertically_up = false ;
+                                    row_step = row ;
+                                }
+
+                            }
+                            while(not_done_checking_vertically_down){
+                                row_step-- ;
+                                if(row_step >= 0 && gridview[row_step][column].state == side){
+                                    count++ ;
+                                    if(count == 5){
+                                        // game is over, stop this process immediatelly
+                                        is_morpion = true ;
+                                        System.out.println("ok it is Morpion here 4");
+                                        break ;
+                                    }
+                                }
+                                else{
+                                    // stop this loop and go check vertically
+                                    not_done_checking_vertically_down = false ;
+                                }
+
+                            }
+                        }
+
+                        //code to check if it is aligned diagonally - right
+                        if(!is_morpion){
+                            count = 1;
+                            int row_step = row ;
+                            int column_step = column ;
+                            while(not_done_checking_diagonally_up_right){
+                                row_step++;
+                                column_step--;
+                                if( (row_step <= 9 && column_step >= 0) && gridview[row_step][column_step].state == side){
+                                    count++ ;
+                                    if(count == 5){
+                                        is_morpion = true ;
+                                        System.out.println("ok it is Morpion here 5");
+                                        break ;
+                                    }
+                                }
+                                else{
+                                    // stop this loop and go check for left
+                                    not_done_checking_diagonally_up_right = false ;
+                                    row_step = row ;
+                                    column_step = column;
+                                }
+
+                            }
+                            while(not_done_checking_diagonally_up_left){
+                                row_step-- ;
+                                column_step++;
+                                if(row_step >= 0 && column_step <= 9 && gridview[row_step][column].state == side){
+                                    count++ ;
+                                    if(count == 5){
+                                        // game is over, stop this process immediatelly
+                                        is_morpion = true ;
+                                        System.out.println("ok it is Morpion here 6");
+                                        break ;
+                                    }
+                                }
+                                else{
+                                    // stop this loop and go check vertically
+                                    not_done_checking_diagonally_up_left = false ;
+                                }
+
+                            }
+                        }
+
+                        //code to check if it is aligned diagonally - left
+                        if(!is_morpion){
+                            count = 1;
+                            int row_step = row ;
+                            int column_step = column ;
+                            while(not_done_checking_diagonally_down_right){
+                                row_step++;
+                                column_step++;
+                                if( (row_step <= 9 && column_step <= 9) && gridview[row_step][column_step].state == side){
+                                    count++ ;
+                                    if(count == 5){
+                                        is_morpion = true ;
+                                        System.out.println("ok it is Morpion here 7");
+                                        break ;
+                                    }
+                                }
+                                else{
+                                    // stop this loop and go check for left
+                                    not_done_checking_diagonally_down_right = false ;
+                                    row_step = row ;
+                                    column_step = column;
+                                }
+
+                            }
+                            while(not_done_checking_diagonally_down_left){
+                                row_step-- ;
+                                column_step--;
+                                if((row_step >=0 && column_step >=0)){
+                                    // System.out.println("the row_step " + row_step + "the column_step " + column_step);
+                                    if(gridview[row_step][column_step].state == side){
+                                        count++ ;
+                                        if(count == 5){
+                                            // game is over, stop this process immediatelly
+                                            is_morpion = true ;
+                                            System.out.println("ok it is Morpion here 8");
+                                            break ;
+                                        }
+                                    }
+
+                                }
+                                else{
+                                    // stop this loop and go check vertically
+                                    not_done_checking_diagonally_down_left = false ;
+                                }
+
+                            }
+                        }
+                    }
+                });
+        thread.start();
+
+
+
+
+
+
+
+        return is_morpion ;
+
     }
 
 
--- app/src/main/java/com/example/jonathan/morpion/square.java
+++ app/src/main/java/com/example/jonathan/morpion/square.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.shapes.Shape;
+import android.os.AsyncTask;
 import android.view.MotionEvent;
 import android.view.View;
 import com.example.jonathan.morpion.game_screen.* ;
@@ -26,10 +27,20 @@ public class Square extends View {
     this variable is boolean type that will identify whether this square has already been taken
      */
     boolean is_tapped = false ;
+    /*
+    position of sqaure in the matrix
+     */
+    int row_position ;
+    int column_position ;
 
     //Constructor
-    public Square(Context context) {
+    public Square(Context context ) {
         super(context);
+    }
+    public Square(Context context, int row, int column ) {
+        super(context);
+        this.row_position = row ;
+        this.column_position = column ;
     }
 
     public void setState(int state) {
@@ -43,17 +54,27 @@ public class Square extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent e) {
-        int player = game_screen.current_player ;
-        if(e.getAction() == MotionEvent.ACTION_DOWN){
-            if(player == 1){
-                this.setBackgroundColor(Color.BLUE);
-                game_screen.current_player = 2 ;
+        if(!this.is_tapped){
+            int player = game_screen.current_player ;
+            if(e.getAction() == MotionEvent.ACTION_DOWN){
+                if(player == 1){
+                    this.setBackgroundColor(Color.BLUE);
+                    this.setState(1);
+                    game_screen.current_player = 2 ;
+                }
+                else{
+                    this.setBackgroundColor(Color.GREEN);
+                    this.setState(2);
+                    game_screen.current_player = 1 ;
+                }
             }
-            else{
-                this.setBackgroundColor(Color.GREEN);
-                game_screen.current_player = 1 ;
-            }
+            this.is_tapped = true ;
+                    game_screen.isMorpion(this.row_position, this.column_position, this.state);
+                    //System.out.println(game_screen.thread_morpion.getState());
+
+
         }
+
         return true ;
     }
 
